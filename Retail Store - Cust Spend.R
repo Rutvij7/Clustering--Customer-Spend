f11434bd5f9d79153450ab540ead1379122d3d73
Rutvij <rutvij.bhutaiya@gmail.com>
2019-05-29 15:21:15 +0530

    UPDATED CLuster with 3 Clusters

--- Retail Store - Cust Spend.R
+++ Retail Store - Cust Spend.R
@@ -10,15 +10,19 @@ attach(customer)
 dim(spend)
 # [1] 10  7
 
+str(customer)
+
 summary(spend)
 
 hist(Avg_Mthly_Spend, col = c('seagreen'))
 
-# >>>>>>  ADD MORE EDA FEATURES <<<<<<<<
+# NO sign of any outlier
+boxplot(Avg_Mthly_Spend)
+
 
 ##  SCALING
 
-cust.scale = customer[,c(3,7)]
+cust.scale = customer[,c(3:7)]
 cust.scale = scale(cust.scale)
 
 distance = dist(cust.scale, method = 'euclidean')
@@ -29,17 +33,19 @@ clust = hclust(distance, method = 'average')
 plot(clust, labels = as.character(customer[,2]))
 clust$height
 
-rect.hclust(clust, k=4, border = 'green')
+rect.hclust(clust, k=3, border = 'green')
 
 ## CLUSTER PROFILING 
 
-customer$cluster = cutree(clust, k=4)
+customer$cluster = cutree(clust, k=3)
 
 avg = aggregate(customer[, -c(1,2,8)], list(customer$cluster), mean)
 
 clust.profile = data.frame(cluster = avg[,1],
                            freq = as.vector(table(customer$cluster)),
                            avg[,-1])
+
+print(clust.profile, digits = 3)
 View(clust.profile)
 write.table(clust.profile,file = 'Cust_Spend_Output_table.csv', sep = ',')
 
